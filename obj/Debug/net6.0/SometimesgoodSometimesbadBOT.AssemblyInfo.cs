b28eadaeeee8dafbaddc6123ae42da72869dbf2a
BlueBeardone <barend.army@gmail.com>
2025-09-27 14:38:23 +0200

    Gl

--- obj/Debug/net6.0/SometimesgoodSometimesbadBOT.AssemblyInfo.cs
+++ obj/Debug/net6.0/SometimesgoodSometimesbadBOT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SometimesgoodSometimesbadBOT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3add13a91acee7230fe857fed2f318a9405df8c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e89378ed5194c6e54f5d2b17eb9aeb833833abb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SometimesgoodSometimesbadBOT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SometimesgoodSometimesbadBOT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
