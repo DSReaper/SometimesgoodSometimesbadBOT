41f53f8326d02d712d9fecd737603bd264ce8cb6
BlueBeardone <barend.army@gmail.com>
2025-09-27 11:07:42 +0200

    Changed color

--- Crazy.cs
+++ Crazy.cs
@@ -22,11 +22,11 @@ public class Crazy : Bot
     // Called when a new round is started -> initialize and do some movement
     public override void Run()
     {
-        BodyColor = Color.FromRgb(0x00, 0xC8, 0x00);   // lime
-        TurretColor = Color.FromRgb(0x00, 0x96, 0x32); // green
-        RadarColor = Color.FromRgb(0x00, 0x64, 0x64);  // dark cyan
-        BulletColor = Color.FromRgb(0xFF, 0xFF, 0x64); // yellow
-        ScanColor = Color.FromRgb(0xFF, 0xC8, 0xC8);   // light red
+        BodyColor = Color.Blue;   // lime
+        TurretColor = Color.Red; // green
+        RadarColor = Color.Black;  // dark cyan
+        BulletColor = Color.OrangeRed; // yellow
+        ScanColor = Color.Yellow;   // light red
 
         movingForward = true;
 
