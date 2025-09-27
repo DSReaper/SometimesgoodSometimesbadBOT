117442dcbe2ad60849605c10dacc06adc524be57
BlueBeardone <barend.army@gmail.com>
2025-09-27 10:58:08 +0200

    initial

--- Crazy/obj/Debug/net6.0/Crazy.AssemblyInfo.cs
+++ Crazy/obj/Debug/net6.0/Crazy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b6e7b7ea33ab57f1db4aedf52638355525be72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e6ccb693f658ec3fd4e6551e090da24f928207b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crazy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
